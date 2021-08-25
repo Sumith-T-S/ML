d63fbb9991e417d8ccf8b5530489c67ecba7bd85
Sumith-T-S <72555167+Sumith-T-S@users.noreply.github.com>
2021-08-25 10:15:08 +0530

    Update 04_readPDF.py

--- MY WORKS/Deep_learning/Optical Character Recognition/04_readPDF.py
+++ MY WORKS/Deep_learning/Optical Character Recognition/04_readPDF.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 18 08:56:53 2020
-
-@author: vishnu
-"""
-
 import io
 from PIL import Image
 import pytesseract
 from wand.image import Image as wi
 
-pdf = wi(filename = "/home/vishnu/Downloads/Workouts ML/02.Deep Learning/Optical Character Recognition/data/Resume.pdf", resolution = 300)
+pdf = wi(filename = "/home/sumith/Downloads/Workouts ML/02.Deep Learning/Optical Character Recognition/data/Resume.pdf", resolution = 300)
 pdfImage = pdf.convert('jpeg')
 
 imageBlobs = []
